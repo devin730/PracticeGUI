bae110d2b62122c4fbd05661db155c06fe553796
Zengming.Deng <deng.zengming@imprexion.com.cn>
2020-03-18 20:51:36 +0800

    project is developed, and add a wx example.

--- project/Login.py
+++ project/Login.py
@@ -12,6 +12,7 @@
 
 import wx
 import wx.adv
+import dia
 
 
 class programme(wx.Frame):
@@ -43,8 +44,9 @@ class programme(wx.Frame):
         #! 一个垂直方向的vbox 起始点
 
         #*写一下登录界面的文本
-        self.fontChinese = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, 'Microsoft yahei')
-        self.fontChinese.SetPointSize(15)
+        self.fontChinese = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False,
+                                   'Microsoft yahei')
+        self.fontChinese.SetPointSize(10)
         hboxAD = wx.BoxSizer(wx.HORIZONTAL)
         ADwords = wx.StaticText(panel, label='这是一个用于XXX的登录页面，请在下方方框内输入信息！')
         ADwords.SetFont(self.fontChinese)
@@ -59,8 +61,8 @@ class programme(wx.Frame):
         st1 = wx.StaticText(panel, label='输入姓名:')
         st1.SetFont(self.fontChinese)
         hbox1.Add(st1, flag=wx.RIGHT, border=10)
-        tc = wx.TextCtrl(panel)
-        hbox1.Add(tc, proportion=1)
+        self.tc = wx.TextCtrl(panel)
+        hbox1.Add(self.tc, proportion=1)
         vbox.Add(hbox1,
                  flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP,
                  border=10)
@@ -70,31 +72,59 @@ class programme(wx.Frame):
         st2 = wx.StaticText(panel, label='输入学号:')
         st2.SetFont(self.fontChinese)
         hbox2.Add(st2, flag=wx.RIGHT, border=10)
-        tc2 = wx.TextCtrl(panel)
-        hbox2.Add(tc2, proportion=1)
+        self.tc2 = wx.TextCtrl(panel)
+        hbox2.Add(self.tc2, proportion=1)
         vbox.Add(hbox2,
                  flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP,
                  border=10)
 
         vbox.Add((-1, 10))
+
+        #! 增加两个按钮OK和Cancel
+        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+        btn1 = wx.Button(panel, label='确定', size=(70, 30))
+        btn2 = wx.Button(panel, label='取消', size=(70, 30))
+        btn1.Bind(wx.EVT_BUTTON, self.Next)
+        btn2.Bind(wx.EVT_BUTTON, self.Close)
+        hbox3.Add(btn1)
+        hbox3.Add(btn2, flag=wx.LEFT | wx.BOTTOM, border=5)
+        vbox.Add(hbox3, flag=wx.RIGHT | wx.ALIGN_RIGHT, border=10)
         panel.SetSizer(vbox)
+
+    def Next(self, e):
+        # 到下一个页面
+        #! 绑定文本框的输入
+        self.userName = self.tc.GetValue()
+        self.userID = self.tc2.GetValue()
+        comfirm_dialogs = dia.DialogMSG(None, user_name=self.userName, uid=self.userID)
+        comfirm_dialogs.Show()
+
+    def Close(self, e):
+        dial = wx.MessageDialog(None, 'Are you sure to quit?', 'Question',
+                                wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
+        ret = dial.ShowModal()
+        if ret == wx.ID_YES:
+            self.Destroy()
+        else:
+            e.Veto()
 
     def OnAboutBox(self, e):
         description = "Hala Madrid"
         licence = "测试代码：授权说明"
         info = wx.adv.AboutDialogInfo()
         info.SetIcon(wx.Icon('../images/rm.jpg', wx.BITMAP_TYPE_ANY))
-        info.SetName('File Hunter')
+        info.SetName('测试程序')
         info.SetVersion('1.0')
         info.SetDescription(description)
-        info.SetCopyright('(C) 2007 - 2018 Jan Bodnar')
-        info.SetWebSite('http://www.zetcode.com')
+        info.SetCopyright('(C) 2020 - Forever, Devin')
+        info.SetWebSite('http://www.dzm.me')
         info.SetLicence(licence)
-        info.AddDeveloper('Jan Bodnar')
-        info.AddDocWriter('Jan Bodnar')
-        info.AddArtist('The Tango crew')
-        info.AddTranslator('Jan Bodnar')
+        info.AddDeveloper('Deng Zengming')
+        info.AddDocWriter('Deng Zengming')
+        info.AddArtist('Deng Zengming')
+        info.AddTranslator('Deng Zengming')
         wx.adv.AboutBox(info)
+
 
 if __name__ == '__main__':
     app = wx.App()
---
+++ project/dia.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# coding: utf-8
+#
+# @module dia
+#
+# @author: Zengming Deng
+#
+# @description:
+#
+# @since: 2020-03-18 11:02:41
+# -------------------------------
+
+import wx
+
+class DialogMSG(wx.Frame):
+    def __init__(self, parent, user_name, uid):
+        super(DialogMSG, self).__init__(parent)
+        self.SetNameID(user_name, uid)
+        self.InitUI()
+        self.SetSize((500, 180))
+        self.SetTitle('确认输入信息')
+        self.Centre()
+
+    def SetNameID(self, username='default name', u_id='0'):
+        self.user_name = username
+        self.user_id = u_id
+
+    def InitUI(self):
+        panel = wx.Panel(self)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        
+        text_user_info_name = "您输入的姓名是 " + self.user_name
+        text_user_info_id = "您输入的ID是 " + self.user_id
+        text_guide = "如果正确请点击确定，如果错误请点击取消，可返回上一级修改。"
+        stext_name = wx.StaticText(panel, label=text_user_info_name)
+        stext_id = wx.StaticText(panel, label=text_user_info_id)
+        stext_guide = wx.StaticText(panel, label=text_guide)
+        vbox.Add(stext_name, flag=wx.RIGHT, border=10)
+        vbox.Add((-1, 10))
+        vbox.Add(stext_id, flag=wx.RIGHT, border=10)
+        vbox.Add((-1, 10))
+        vbox.Add(stext_guide, flag=wx.RIGHT, border=10)
+        vbox.Add((-1, 10))
+        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+        btn1 = wx.Button(panel, label='确定', size=(70, 30))
+        btn2 = wx.Button(panel, label='取消', size=(70, 30))
+        btn1.Bind(wx.EVT_BUTTON, self.Next)
+        btn2.Bind(wx.EVT_BUTTON, self.Close)
+        hbox3.Add(btn1)
+        hbox3.Add(btn2, flag=wx.LEFT | wx.BOTTOM, border=5)
+        vbox.Add(hbox3, flag=wx.RIGHT | wx.ALIGN_RIGHT, border=10)
+        panel.SetSizer(vbox)
+
+    def Next(self, e):
+        # 到下一个页面
+        self.Destroy()
+        return 100
+
+    def Close(self, e):
+        dial = wx.MessageDialog(None, '返回上一级修改?', 'Question',
+                                wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION)
+        ret = dial.ShowModal()
+        if ret == wx.ID_YES:
+            self.Destroy()
+        else:
+            e.Veto()
+
+if __name__ == '__main__':
+    app = wx.App()
+    ex = DialogMSG(None, user_name='Devin', uid='2026')
+    ex.Show()
+    app.MainLoop()
